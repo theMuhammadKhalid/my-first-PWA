f4cfb5b19b69fc3b7f0ab111fa4ee74a9cf84a1d
Muhammad Khalid <themuhammadkhalid@gmail.com>
2021-10-28 21:39:19 +0500

    First

---
+++ main.js
@@ -0,0 +1,12 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((reg) => {
+        console.log("Registered! ", reg);
+      })
+      .catch((err) => {
+        console.log("Registeration failed: ", err);
+      });
+  });
+}
---
+++ sw.js
@@ -0,0 +1,23 @@
+const cacheName = "cache-v1";
+const resourcesToPrecache = ["/", "index.html"];
+
+self.addEventListener("install", (event) => {
+  console.log("Service worker install event!");
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(resourcesToPrecache);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Activate event!");
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
