c7d8968307b7918ece6b486bc282c10018f66112
Muhammad Khalid <themuhammadkhalid@gmail.com>
2021-11-05 20:43:58 +0500

    Add notification code

--- index.js
+++ index.js
@@ -11,27 +11,12 @@ if ("serviceWorker" in navigator) {
   });
 }
 
-// let deferredPrompt;
-// window.addEventListener("beforeinstallprompt", (e) => {
-//   // Prevent from automatically showing the prompt
-//   e.preventDefault();
-//   deferredPrompt = e;
+Notification.requestPermission().then((result) => {
+  if (result === "granted") {
+    randomNotification();
+  }
+});
 
-//   // Update UI notify the user they can add to home screen
-//   btnAdd.style.display = "block";
-//   btnAdd.addEventListener("click", () => {
-//     deferredPrompt.prompt();
-//     deferredPrompt.userChoice.then((choiceResult) => {
-//       if (choiceResult.outcome === "accepted") {
-//         console.log("User accepted the A2HS prompt");
-//       }
-//       deferredPrompt = null;
-//     });
-//   });
-// });
-
-// window.addEventListener("appinstalled", (evt) => {
-//   app.logEvent("a2hs", "installed");
-// });
-
-// window.matchMedia("(display-mode: standalone)");
+function randomNotification() {
+  new Notification("Hi, This is my first PWA");
+}
