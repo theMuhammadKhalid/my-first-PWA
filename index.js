2bd13178d305dbc9cf2a8db88ea199be3541e308
Muhammad Khalid <themuhammadkhalid@gmail.com>
2021-11-05 18:36:11 +0500

    Updated

--- index.js
+++ index.js
@@ -11,27 +11,27 @@ if ("serviceWorker" in navigator) {
   });
 }
 
-let deferredPrompt;
-window.addEventListener("beforeinstallprompt", (e) => {
-  // Prevent from automatically showing the prompt
-  e.preventDefault();
-  deferredPrompt = e;
+// let deferredPrompt;
+// window.addEventListener("beforeinstallprompt", (e) => {
+//   // Prevent from automatically showing the prompt
+//   e.preventDefault();
+//   deferredPrompt = e;
 
-  // Update UI notify the user they can add to home screen
-  btnAdd.style.display = "block";
-  btnAdd.addEventListener("click", () => {
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === "accepted") {
-        console.log("User accepted the A2HS prompt");
-      }
-      deferredPrompt = null;
-    });
-  });
-});
+//   // Update UI notify the user they can add to home screen
+//   btnAdd.style.display = "block";
+//   btnAdd.addEventListener("click", () => {
+//     deferredPrompt.prompt();
+//     deferredPrompt.userChoice.then((choiceResult) => {
+//       if (choiceResult.outcome === "accepted") {
+//         console.log("User accepted the A2HS prompt");
+//       }
+//       deferredPrompt = null;
+//     });
+//   });
+// });
 
-window.addEventListener("appinstalled", (evt) => {
-  app.logEvent("a2hs", "installed");
-});
+// window.addEventListener("appinstalled", (evt) => {
+//   app.logEvent("a2hs", "installed");
+// });
 
-window.matchMedia("(display-mode: standalone)");
+// window.matchMedia("(display-mode: standalone)");
